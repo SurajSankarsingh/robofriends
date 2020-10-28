e9f580d0148d316fbf983a64cb6a3e915fdbc690
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2020-10-28 09:20:27 -0400

    added ErrorBoundary component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "./App.css";
 
-import { requestRobots, setSearchField } from '../actions';
+import { requestRobots, setSearchField } from "../actions";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error
-  }
-}
+    error: state.requestRobots.error,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 class App extends Component {
-  
-
   componentDidMount() {
     this.props.onRequestRobots();
   }
 
   render() {
     const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
+    const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
-    return isPending ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={onSearchChange}/>
-          <Scroll>
+    });
+    return isPending ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
             <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
 }
 
